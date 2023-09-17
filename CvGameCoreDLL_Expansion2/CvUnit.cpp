ec2b0e20373a9ddb8a323317aad6e1faad1f7d50
TechpriestEnginseer <techpriest8@gmail.com>
2023-09-16 18:17:32 -0700

    Added AI Promotion Support + Text Change for past Proposals (XGN 10202)
    * Added AI Promotion Support + Text Change for past Proposals
    
    * Fixed some pixelation with [ICON_AIRPORT] and [ICON_AIRSTRIKE_DEFENSE]

--- CvGameCoreDLL_Expansion2/CvUnit.cpp
+++ CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -3223,6 +3223,12 @@ bool CvUnit::getCaptureDefinition(CvUnitCaptureDefinition* pkCaptureDef, PlayerT
 			// (5-82): Testing behavior, they will pillage to regenerate some lost HP or try to retreat if needed.
 			if (MOD_BALANCE_VP)
 			{
+				// Unused code, but might be used in next Congress
+				// allows captured units to gain XP off its origin city (of its capturer which is either the nearest city or the Capital City!)
+				//CvCity* pOriginCityCaptured = pkCapturedUnit->getOriginCity();
+				//if (pOriginCityCaptured == NULL)
+				//	pOriginCityCaptured = GET_PLAYER(kCaptureDef.eCapturingPlayer).getCapitalCity();
+				//pOriginCityCaptured->addProductionExperience(pkCapturedUnit, false, true);
 				pkCapturedUnit->restoreFullMoves();
 				pkCapturedUnit->setMadeAttack(true);
 				pkCapturedUnit->SetTurnProcessed(false);
@@ -31884,12 +31890,37 @@ int CvUnit::AI_promotionValue(PromotionTypes ePromotion)
 	// nM: +10 Pincer (Boarding Party) 4.	
 	if(iTemp > 0)
 	{
-
 		iExtra  = (iTemp) * ( iFlavorDefense + iFlavorOffense + iFlavorMobile);
 		iExtra *= 100 + GetFlankAttackModifier();
 		iExtra /= 100;
 		iExtra *= 0.5;
 		iExtra *= (2 + baseMoves(false)) / 2;    		// gives max moves, false = not embarked	
+		iValue += iExtra;
+	}
+
+	iTemp = pkPromotionInfo->GetExtraFlankPower();
+	// Screening: Only recon units can use it now however, so it's not that high of a value.
+	// If you have access to flanking bonuses however.. always choose this over other flanking promotions!
+	if (iTemp > 0)
+	{
+		iExtra = (iTemp) * (iFlavorOffense + iFlavorMobile + iFlavorRecon);
+		iExtra *= 100 + GetFlankAttackModifier();
+		iExtra /= 100;
+		iExtra *= (2 + baseMoves(false)) / 2;    		// gives max moves, false = not embarked	
+		iValue += iExtra;
+	}
+
+	if (pkPromotionInfo->IsRangedFlankAttack() && !IsRangedFlankAttack() && IsCanAttackRanged())
+	// Envelopment: Only available to Skirmishers (Mounted Ranged)
+	// This is really strong, but if my ranged attacks exceed 1 range, I can't use this as effectively.
+	{
+		iExtra = (iFlavorOffense + iFlavorMobile);
+		iExtra *= 2;
+		iExtra *= max(1, baseMoves(false));
+		if (canMoveAfterAttacking())
+			iExtra *= 5;
+		if (GetRange() > 1)
+			iExtra /= 10;
 		iValue += iExtra;
 	}
 
