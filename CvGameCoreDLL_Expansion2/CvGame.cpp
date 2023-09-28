541063de53759afb437b999edf098422023973de
Daniil Sivak <seroperson@gmail.com>
2023-09-29 01:18:54 +0500

    Split TradeRoutePathCache into UI and DLL instances (#10250)

--- CvGameCoreDLL_Expansion2/CvGame.cpp
+++ CvGameCoreDLL_Expansion2/CvGame.cpp
@@ -129,6 +129,7 @@ CvGame::CvGame() :
 	, m_bSavedOnce(false)
 #endif
 	, m_bArchaeologyTriggered(false)
+	, m_bIsDesynced(false)
 	, m_lastTurnAICivsProcessed(-1)
 	, m_processPlayerAutoMoves(false)
 	, m_cityDistancePathLength(NO_DOMAIN) //for now!
@@ -1166,6 +1167,7 @@ void CvGame::uninit()
 	m_bEverRightClickMoved = false;
 	m_bCombatWarned = false;
 	m_bArchaeologyTriggered = false;
+	m_bIsDesynced = false;
 
 	m_eHandicap = NO_HANDICAP;
 	m_ePausePlayer = NO_PLAYER;
@@ -5981,6 +5983,22 @@ bool CvGame::isSimultaneousTeamTurns() const
 	return true;
 }
 
+
+bool CvGame::isDesynced() const
+{
+	if (!isNetworkMultiPlayer())
+		return false;
+
+	return m_bIsDesynced;
+}
+void CvGame::setDesynced(bool bNewValue)
+{
+	if (!isNetworkMultiPlayer())
+		return;
+
+	m_bIsDesynced = bNewValue;
+}
+
 //	--------------------------------------------------------------------------------
 bool CvGame::isFinalInitialized() const
 {
@@ -8800,9 +8818,19 @@ void CvGame::doTurn()
 
 	LogGameState();
 
-	//autosave after doing a turn
 	if (isNetworkMultiPlayer())
+	{
+		//autosave after doing a turn
 		gDLL->AutoSave(false, false);
+
+		// send desync warning, if applicable
+		if (isDesynced())
+		{
+			setDesynced(false);
+			CvString strWarningText = GetLocalizedText("TXT_KEY_VP_MP_WARNING_DESYNC");
+			gGlobals.getDLLIFace()->sendChat(strWarningText, CHATTARGET_ALL, NO_PLAYER);
+		}
+	}
 }
 
 //	--------------------------------------------------------------------------------
